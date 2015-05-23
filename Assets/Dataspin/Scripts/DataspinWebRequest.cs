8bfa3312451dd981920b053cdb1ee194ac851b97
Rafal Wilinski <rafal@dataspin.io>
2015-05-23 12:10:23 +0200

    DataspinWebRequest android hotfix

--- Assets/Dataspin/Scripts/DataspinWebRequest.cs
+++ Assets/Dataspin/Scripts/DataspinWebRequest.cs
@@ -151,12 +151,12 @@ namespace Dataspin {
 			// }
 
 			//Re-assign sessionId in case when session has been invalidated and new ID has been assigned
-			this.postData["session"] = DataspinManager.Instance.SessionId;
+			//this.postData["session"] = DataspinManager.Instance.SessionId;
 
 			#if UNITY_ANDROID && !UNITY_EDITOR
-
 				this.externalTaskPid = UnityEngine.Random.Range(0,100000000).ToString() + "-" + UnityEngine.Random.Range(0,100000000).ToString() + "-" + UnityEngine.Random.Range(0,100000000).ToString();
 				DataspinManager.Instance.StartExternalTask(this);
+				yield return new WaitForSeconds(0.0001f);
 
 			#else
 
